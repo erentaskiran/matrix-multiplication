6f57e04b8e2f7f1cb7cc92bd1e5888b303aa7971
erentaskiran <101654590+erentaskiran@users.noreply.github.com>
2023-11-06 12:50:19 +0300

    Initial Commit

---
+++ matrisCarpim/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  matrisCarpim
+//
+//  Created by Eren Taşkıran on 6.11.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
